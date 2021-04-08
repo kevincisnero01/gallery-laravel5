c21b34eb438e8ba831b7335b90605ddf951319a6
kevincisnero01 <kevingabriel18@gmail.com>
2021-04-08 00:27:34 -0430

    crear migraciones para nuestros modelos

--- database/migrations/2021_04_08_042950_CrearTablaFotos.php
+++ database/migrations/2021_04_08_042950_CrearTablaFotos.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration {
+class CrearTablaFotos extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,15 +12,13 @@ class CreateUsersTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('usuarios', function(Blueprint $table)
+		Schema::create('fotos', function(Blueprint $table)
 		{
 			$table->increments('id');
-			$table->string('name');
-			$table->string('email')->unique();
-			$table->string('password', 60);
-			$table->string('pregunta');
-			$table->string('respuesta');
-			$table->rememberToken();
+			$table->string('nombre');
+			$table->string('descripcion');
+			$table->integer('album_id')->unsigned();
+			$table->foreign('album_id')->references('id')->on('albunes');
 			$table->timestamps();
 		});
 	}
@@ -32,7 +30,7 @@ class CreateUsersTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('users');
+		Schema::drop('fotos');
 	}
 
 }
